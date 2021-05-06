4d50b8b6af38d701a4062192c739be848fd27cd0
Joe Taylor <joetaylorconsulting@gmail.com>
2021-05-06 17:54:21 +0200

    Simplify and test inspect_based_repr

--- wysdom/repr.py
+++ wysdom/repr.py
@@ -2,11 +2,7 @@ import inspect
 
 
 def inspect_based_repr(object_to_repr: object):
-    if not hasattr(object_to_repr, "__init__"):
-        return object.__repr__(object_to_repr)
     arg_spec = inspect.getfullargspec(object_to_repr.__init__)
-    if arg_spec.varargs or arg_spec.varkw:
-        return object.__repr__(object_to_repr)
     arg_parts = [
         f"{arg}={repr(getattr(object_to_repr, arg))}"
         for arg in list(arg_spec.args[1:]) + list(arg_spec.kwonlyargs)
